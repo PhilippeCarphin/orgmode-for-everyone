ea29c249347296dc064f8dae66a30da7e16a4d0e
Philippe Carphin <philippe.carphin2@canada.ca>
2021-04-11 00:30:10 -0400

    Actions for intro

---
+++ Intro_script.sh
@@ -0,0 +1,17 @@
+# Initial actions
+vim README.md # YUCK
+./decorated.sh # press no
+vim README.md # Better but TOC is not enough, my brain and seven things etc
+./decorated.sh # Answer y
+vim README.md # All folded is the best!
+# Talk more about folding"
+
+# Open up the same file in org
+vim README.org # It's nice
+
+# Move on to show orgmode syntax in a new file
+# In vim:
+:b basics.org
+
+
+
